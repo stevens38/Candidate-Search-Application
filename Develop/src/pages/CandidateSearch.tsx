abb496c498b52813e9d05ef0f62e1ed13277e7d5
JStevens <stevens38@marshall.edu>
2024-11-19 13:58:59 -0500

    Candidate.search updates and changes

--- Develop/src/pages/CandidateSearch.tsx
+++ Develop/src/pages/CandidateSearch.tsx
@@ -2,8 +2,46 @@ import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
 
 const CandidateSearch = () => {
-  return <h1>CandidateSearch</h1>;
+  const [candidates, setCandidates] = useState<{ login: string }[]>([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentUser, setCurrentUser] = useState({name: '', email: '', bio: '', company: '', location: ''});
+  useEffect(() => {
+    searchGithub().then((data) => {
+      setCandidates(data);
+    });
+  }, []);  
+  console.log(candidates);
+useEffect(() => {
+  if (candidates.length === 0) return;
+  searchGithubUser(candidates[currentIndex].login).then((data) => {
+    console.log(data);
+    setCurrentUser(data);
+  });
+}, [currentIndex, candidates]);
+console.log(currentUser);
+
+  return (
+    <div>
+      <h1>CandidateSearch</h1>
+      <div>
+        <h3>{currentUser.name ||"n/a"}</h3>
+        <p>{currentUser.email ||"n/a"}</p>
+        <p>{currentUser.bio ||"n/a"}</p>
+        <p>{currentUser.company ||"n/a"}</p>
+        <p>{currentUser.location ||"n/a"}</p>
+        <button onClick={() => setCurrentIndex((prev) => prev + 1)}>Next</button>
+        </div>
+    </div>
+  );
 };
+
+
+//set up state for github user data
+
+
+//set up state for current user index
+//set up use affect to call searchGithub when the component mounts
+
 
 
 export default CandidateSearch;
