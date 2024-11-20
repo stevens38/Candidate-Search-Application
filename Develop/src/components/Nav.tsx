33cc49a5551e080da02c552ac0a2dc3687a8cf49
JStevens <stevens38@marshall.edu>
2024-11-20 18:14:52 -0500

    Nav.tsx updated

--- Develop/src/components/Nav.tsx
+++ Develop/src/components/Nav.tsx
@@ -1,8 +1,13 @@
-const Nav = () => {
-  // TODO: Add necessary code to display the navigation bar and link between the pages
-  return (
-    <div>Nav</div>
-  )
-};
 
-export default Nav;
+  import { Link } from 'react-router-dom';
+  const Nav = () => {
+    // TODO: Add necessary code to display the navigation bar and link between the pages
+    return (
+      <div className="nav" style={{wordSpacing: "1px", justifyContent:"space-evenly", width: "15%", display
+      :"flex", flexWrap: "wrap"}}>
+        <Link to="/">Home</Link>
+        <Link to="/SavedCandidates">Potential Candidates</Link>
+      </div>
+    )
+  };
+  export default Nav;
